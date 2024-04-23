42c27b2feb009edc3513413fad96038934ac3642
jordanrhym <143748083+jordanrhym@users.noreply.github.com>
2024-04-23 16:10:55 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 function myFunction() {
     var x = document.getElementById("myLinks");
+    console.log("x:", x);
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
   }
+  document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('click', event => {
+        console.log('Link clicked');
+        var x = document.getElementById("myLinks");
+        x.style.display = "none";
+    });
+});
 
   var slideIndex = [1,1,1,1];
   var slideId = ["666Slides", "ParSlides", "606Slides", "flameSlides"]
@@ -14,17 +22,32 @@ function myFunction() {
   showDivs(1, 2);
   showDivs(1, 3);
   
-  function plusDivs(n, index) {
-    showDivs(slideIndex[index] += n, index);
+  function plusDivs(n, no) {
+    showDivs(slideIndex[no] += n, no);
   }
   
-  function showDivs(n, index) {
+  function showDivs(n, no) {
     var i;
-    var x = document.getElementsByClassName(slideId[index]);
-    if (n > x.length) {slideIndex[index] = 1}
-    if (n < 1) {slideIndex[index] = x.length}
+    var x = document.getElementsByClassName(slideId[no]);
+    console.log("Selected elements:", x);
+    if (n > x.length) {slideIndex[no] = 1}
+    if (n < 1) {slideIndex[no] = x.length}
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
     }
-    x[slideIndex[index]-1].style.display = "block";  
+    x[slideIndex[no]-1].style.display = "block";  
   }
+
+  document.querySelectorAll('.button-left').forEach(item => {
+    item.addEventListener('click', event => {
+        console.log('Left button clicked');
+        plusDivs(-1, Number(item.getAttribute('data-slideshow-index')));
+    });
+});
+
+document.querySelectorAll('.button-right').forEach(item => {
+    item.addEventListener('click', event => {
+        console.log('Right button clicked');
+        plusDivs(1, Number(item.getAttribute('data-slideshow-index')));
+    });
+});
